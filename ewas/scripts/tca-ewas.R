ea4e2ae55bcd0b0c63e1656a3c1d24ed8fa03d24
Thomas Battram <thomas.battram@bristol.ac.uk>
2022-08-19 11:20:43 +0100

    updated

--- ewas/scripts/tca-ewas.R
+++ ewas/scripts/tca-ewas.R
@@ -16,7 +16,7 @@ args <- commandArgs(trailingOnly = TRUE)
 phen_file <- args[1]
 meth_file <- args[2]
 svs_file <- args[3]
-out_file <- args[4]
+out_files <- args[4]
 max_chunks <- args[5]
 
 # phen_file <- "../data-extraction-and-qc/data/ad-data-cleaned.tsv"
